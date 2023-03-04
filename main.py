fcf3e6364fc1b96aa2640107c5381221d4115221
henriits <121551949+henriits@users.noreply.github.com>
2023-03-04 16:52:45 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+canvas_width = int(input("Enter a canvas width: "))
+canvas_height = int(input("Enter a canvas height: "))
+canvas_color = input("Enter canvas color (white or black): ")
+object_to_draw = input("what do you like to draw? Enter quit to quit. ")  # while true
+object_x_location = int(input(f"Enter x of {object_to_draw}"))
+object_y_location = int(input(f"Enter x of {object_to_draw}"))
+object_width = input(f"Enter the width of the {object_to_draw}")
+object_height = input(f"Enter the height of the {object_to_draw}")
+red = input("How much red should the rectangle have? ")
+green = input("How much green should the rectangle have? ")
+blue = input("How much blue should the rectangle have? ")
