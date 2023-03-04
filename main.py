6ad59f1fa89606069f51e19465e2dd92dc682a03
henriits <121551949+henriits@users.noreply.github.com>
2023-03-04 19:15:54 +0200

    finishing project

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from canvas import Canvas
 from shapes import Square, Rectangle
 
-# canvas = Canvas(height=20,width=30,color=(255,255,255))
-# r1 = Rectangle(x=1,y=6,height=7,width=10,color=(100,200,125))
-# r1.draw(canvas)
-# s1 = Square(x=1,y=3,side=3,color=(0,100,222))
-# s1.draw(canvas)
-# canvas.make("canvas1.png")
+canvas_width = int(input("Enter a canvas width: "))
+canvas_height = int(input("Enter a canvas height: "))
+canvas_color = input("Enter canvas color (white or black): ")
+if canvas_color == "white":
+    canvas_color = (255, 255, 255)
+elif canvas_color == "black":
+    canvas_color = (0, 0, 0)
+canvas = Canvas(height=canvas_height, width=canvas_width, color=canvas_color)
 
 want_to_draw = True
 while want_to_draw:
-    canvas_width = int(input("Enter a canvas width: "))
-    canvas_height = int(input("Enter a canvas height: "))
-    canvas_color = input("Enter canvas color (white or black): ")
+
     object_to_draw = input("what do you like to draw? Enter quit to quit. ")  # while true
     if object_to_draw == "quit":
         break
@@ -24,15 +24,9 @@ while want_to_draw:
     green = int(input("How much green should the rectangle have?: "))
     blue = int(input("How much blue should the rectangle have?: "))
 
-    if canvas_color == "white":
-        canvas_color = (255, 255, 255)
-    elif canvas_color == "black":
-        canvas_color = (0, 0, 0)
-    canvas = Canvas(height=canvas_height, width=canvas_width, color=canvas_color)
     if object_to_draw == "quit":
-        exit()
+        want_to_draw = False
     elif object_to_draw == "rectangle":
-
         r1 = Rectangle(x=object_x_location, y=object_y_location, height=object_height, width=object_width,
                        color=(red, green, blue))
         r1.draw(canvas)
@@ -41,4 +35,4 @@ while want_to_draw:
         s1 = Square(x=object_x_location, y=object_y_location, side=object_height, color=(red, green, blue))
         s1.draw(canvas)
 
-    canvas.make("canvas1.png")
+canvas.make("canvas1.png")
